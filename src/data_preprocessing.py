71a32e5e1b21f9851cc3b80b92a9a9f1b36fcfb7
Anamikamahi18 <anamikamahi18@gmail.com>
2026-05-15 12:47:18 +0530

    Added preprocessing script

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,8 +1,11 @@
 import pandas as pd
+import numpy as np
 
 
 def data_preprocessing(df):
     # Example preprocessing steps
     df = df.dropna()  # Remove missing values
     df = pd.get_dummies(df, drop_first=True)  # One-hot encode categorical variables
+    df = df.drop_duplicates()
+    df["oldpeak_log"] = np.log1p(df["oldpeak"])
     return df
