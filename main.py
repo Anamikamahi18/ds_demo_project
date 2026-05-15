a87d90fdb7c372bda7bf903d0e8dbd4b34447f21
Anamikamahi18 <anamikamahi18@gmail.com>
2026-05-15 12:42:19 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pandas as pd
 from src.helper import load_config
+from src.data_preprocessing import data_preprocessing
 
 config = load_config()
 raw_data_path = config["raw_data_path"]
 data = pd.read_csv(raw_data_path)
+data = data_preprocessing(data)
+data.to_csv(config["preprocessed_data_path"], index=False)
 print(data.head())
